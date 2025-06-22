9b04fd4dd660473f673a2844e1edd47eac3abc23
Alanwaser22 <alanwaserman@gmail.com>
2025-06-22 18:43:35 -0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
   const { code, shop } = req.query;
 
- const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const redirectUri = process.env.REDIRECT_URI;
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
+  const redirectUri = process.env.REDIRECT_URI;
 
   const response = await fetch(`https://${shop}/admin/oauth/token`, {
     method: "POST",
@@ -18,7 +18,6 @@ const redirectUri = process.env.REDIRECT_URI;
   });
 
   const data = await response.json();
-
   console.log("Token de acceso:", data);
 
   res.status(200).json({ success: true, token: data.access_token });
