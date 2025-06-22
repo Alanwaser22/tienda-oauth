2db20627cad392417138ec6b88df9faad9ec0a76
Alanwaser22 <alanwaserman@gmail.com>
2025-06-22 18:47:08 -0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const tienda = req.query.shop;
-  const clientId = "18797";
-  const redirectUri = "https://tienda-oauth.vercel.app/api/callback";
+  const clientId = process.env.CLIENT_ID;
+  const redirectUri = process.env.REDIRECT_URI;
   const scopes = ["read_orders"];
 
   const url = `https://${tienda}/admin/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scopes.join(",")}`;
